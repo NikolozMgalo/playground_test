a5449f879c1b297480a02593f221659d3ac8a104
NikolozMgalo <nikolozmgalo@gmail.com>
2024-11-20 21:21:40 +0400

    added additional screenshots

--- test/step-definitions/sampleAppSteps.js
+++ test/step-definitions/sampleAppSteps.js
@@ -7,28 +7,30 @@ import AllureReporter from '@wdio/allure-reporter';
 
 
 When('I select Sample App page', async () => {
-    AllureReporter.addStep('I select Sample App page')
+    AllureReporter.addStep('I select Sample App page');
     AllureReporter.addAttachment(await Browser.addScreenshot('example'))
     await SampleAppPage.clickSampleAppButton();
 });
 
 When('I enter username', async () => {
-    AllureReporter.addStep('I enter username')
-    AllureReporter.addSeverity('blocker')
+    AllureReporter.addStep('I enter username');
+    AllureReporter.addSeverity('blocker');
     await SampleAppPage.enterUsername(process.env.USERNAME);
 });
 
 When('I enter password', async () => {
-    AllureReporter.addStep('I enter password')
+    AllureReporter.addStep('I enter password');
     await SampleAppPage.enterPassword(process.env.PASSWORD);
 });
 
 When('I click on login button', async () => {
-    AllureReporter.addStep('I click on login button')
+    AllureReporter.addStep('I click on login button');
+    AllureReporter.addAttachment('screenshot', await Browser.addScreenshot('screenshot'));
     await SampleAppPage.clickLoginButton();
 });
 
 Then('I should see welcome message', async () => {
-    AllureReporter.addStep('I should see welcome message')
+    AllureReporter.addStep('I should see welcome message');
     assert.isTrue(await SampleAppPage.checkLoginStatus(), 'did not login successfully');
+    AllureReporter.addAttachment('screenshot', await Browser.addScreenshot('screenshot'));
 });
